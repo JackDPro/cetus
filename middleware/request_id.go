08c22eb544d57778a1264b0af31b6d63b92ce1b7
Jack <hello@jack.vip>
2025-02-07 16:17:33 +0800

    test

--- middleware/request_id.go
+++ middleware/request_id.go
@@ -2,9 +2,7 @@ package middleware
 
 import (
 	"bytes"
-	"github.com/JackDPro/cetus/provider"
 	"github.com/gin-gonic/gin"
-	"github.com/go-kit/log/level"
 	"github.com/google/uuid"
 )
 
@@ -22,10 +20,12 @@ func RequestId() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		writer := &BodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
 		c.Writer = writer
-		_ = level.Info(provider.GetLogger()).Log("id", "jack", "header", c.Request.Header)
 		requestId := c.GetHeader("HTTP_X_REQUEST_ID")
 		if requestId == "" {
 			requestId = c.GetHeader("HTTP_REQUEST_ID")
+		}
+		if requestId == "" {
+			requestId = c.GetHeader("X-Request-Id")
 		}
 		if requestId == "" {
 			requestId = uuid.New().String()
